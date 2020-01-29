be1203de4e18ee331219b0087a121c4f7d4bb57f
Paul Li <paul.li@amberelectric.com.au>
2020-01-29 21:52:50 +1100

    Added google tag manager, which has configured google analytics

--- clients/absorbance/gatsby-config.js
+++ clients/absorbance/gatsby-config.js
@@ -57,5 +57,20 @@ module.exports = {
         whitelist: Object.keys(env),
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-T8FBMTB',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+      },
+    },
   ],
 }
