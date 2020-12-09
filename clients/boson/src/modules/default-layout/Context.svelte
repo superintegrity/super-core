587e041e7c47368e4bbb7f3ada2ae098cd58436d
Paul Li <paul.li@amberelectric.com.au>
2020-12-10 10:30:54 +1100

    Removed svelte-themer which caused flickering when navigate

--- clients/boson/src/modules/default-layout/Context.svelte
+++ clients/boson/src/modules/default-layout/Context.svelte
@@ -1,21 +1,21 @@
 <!-- Non visual common codes -->
-<svelte:component this={ThemeWrapper} themes={themes}>
-  <slot />
-</svelte:component>
+<!-- <svelte:component this={ThemeWrapper} themes={themes}> -->
+<slot />
 
+<!-- </svelte:component> -->
 <script lang="ts">
   import 'reset-css/reset.css'
   import './layout.css'
-  import { themes } from './themes'
+  // import { themes } from './themes'
   import 'aos/dist/aos.css'
   import AOS from 'aos'
 
   import { onMount } from 'svelte'
-  let ThemeWrapper: typeof import('svelte-themer').ThemeWrapper
+  // let ThemeWrapper: typeof import('svelte-themer').ThemeWrapper
 
   onMount(async () => {
-    const module = await import('svelte-themer')
-    ThemeWrapper = module.ThemeWrapper
+    // const module = await import('svelte-themer')
+    // ThemeWrapper = module.ThemeWrapper
 
     AOS.init({
       duration: 1200,
